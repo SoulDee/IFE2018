4119b2abbaf5642b4ee7d8541097a020f6c89dcf
SoulDee <17746076034@163.com>
2018-05-01 22:04:45 +0800

    updata

--- disigner/no_6/index.js
+++ disigner/no_6/index.js
@@ -1,6 +1,7 @@
 const picPre = document.querySelectorAll(".wrap ul li");
 const picList = document.querySelectorAll(".wrap > img");
-
+let mid = 0;
+let index = 0;
 var slider = {
     // 为图片绑定动画 版本 2.0
     binding: function(data) {
@@ -8,34 +9,38 @@ var slider = {
             let aAnimation = data[i][0];
             let time = data[i][1];
             
-            picPre[i].addEventListener("click", function () {
-                slider.touchAnimation(i, aAnimation, time);
+            picPre[i].addEventListener("click", function(event) {
+                // 获取点击的元素的下标,等价于jquery的list.indexOf(elt)
+                index = [].indexOf.call(picPre, event.target);
+                // 判断是否重复点击，是的话不更改层级，不是的话才更改
+                if (picList[index].classList.contains("show") === true) {
+                    slider.touchAnimation(index, aAnimation, time);
+                }else {
+                    slider.touchAnimation(index, aAnimation, time);
+                    slider.addLevel(index, mid);
+                    mid = index;
+                }
             }, false);
         }
     },
     // 绑定事件，动画
-    touchAnimation: function (i, aAnimation, time) {
-        picList[i].style.animation = aAnimation;
+    touchAnimation: function (index, aAnimation, time) {
+        picList[index].style.animation = aAnimation;
         // 延迟去掉动画，使之能够多次播放
         setTimeout(() => {
-            picList[i].style.animation = '';
+            picList[index].style.animation = '';
         }, time);
-        slider.addLevel();
-        picList[i].classList.add("show");
     },
-    // 增加层次,实现上一张点击的图片会放在中间层次
-    addLevel: function () {
-        // 增加中间层
+    // 增加层次
+    addLevel: function (index, mid) {
+        // 移除所有中间层和顶层
         for (let i = 0; i < picList.length; i++) {
             picList[i].classList.remove("show-mid");
-            if (picList[i].classList.contains("show") === true) {
-                picList[i].classList.add("show-mid");
-            }
-        }
-        // 移除所有上层
-        for (let i = 0; i < picList.length; i++) {
             picList[i].classList.remove("show");
         }
+        // 按照获得的下标绑定
+        picList[index].classList.add("show");
+        picList[mid].classList.add("show-mid");
     }
 }
 // 传入数据版本 v2.0
